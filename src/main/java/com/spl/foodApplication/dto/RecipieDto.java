b96d8e68f0195773b4bcebff77fa3198fa873736
Mary <marysubash74@gmail.com>
2024-11-09 17:09:44 +0530

    added receipe successfully

--- src/main/java/com/spl/foodApplication/dto/RecipieDto.java
+++ src/main/java/com/spl/foodApplication/dto/RecipieDto.java
@@ -1,4 +1,11 @@
 package com.spl.foodApplication.dto;
 
+import lombok.Data;
+
+@Data
 public class RecipieDto {
+
+    private String title;
+    private String description;
+    private String steps;
 }
