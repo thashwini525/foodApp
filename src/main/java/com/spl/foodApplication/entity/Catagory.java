080dfc58ba6ad0a4db51035a4a76ba590d80362e
SimranSultana <simran07sultana@gmail.com>
2024-11-09 10:17:28 +0530

    changes in category

--- src/main/java/com/spl/foodApplication/entity/Catagory.java
+++ src/main/java/com/spl/foodApplication/entity/Catagory.java
@@ -1,4 +1,15 @@
 package com.spl.foodApplication.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Catagory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "category_id")
+    @SequenceGenerator(initialValue = 1,allocationSize = 100,name = "category_id")
+    private int CategoryId;
+    private String CategoryName;
+
 }
