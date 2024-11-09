d559ddf97caf044c3548d162698120a1a06fa388
thashwini <thashwinib@gmail.com>
2024-11-09 11:01:09 +0530

    added some user fields

--- src/main/java/com/spl/foodApplication/service/Impl/UsersServiceImpl.java
+++ src/main/java/com/spl/foodApplication/service/Impl/UsersServiceImpl.java
@@ -1,4 +1,17 @@
 package com.spl.foodApplication.service.Impl;
 
-public class UsersServiceImpl {
+import com.spl.foodApplication.dao.UsersDao;
+import com.spl.foodApplication.dto.ResponseStructure;
+import com.spl.foodApplication.dto.UsersDto;
+import com.spl.foodApplication.entity.Users;
+import com.spl.foodApplication.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+public class UsersServiceImpl implements UsersService {
+    private UsersDao usersDao;
+    @Override
+    public ResponseEntity<ResponseStructure<Users>> saveUsers(UsersDto usersDto) {
+return null;
+    }
 }
